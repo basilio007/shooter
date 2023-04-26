020b6891f56f44726e57ec35375d76b894125500
basilio007 <113118989+basilio007@users.noreply.github.com>
2023-04-26 20:17:22 +0200

    some promblem

--- johnlenon/Assets/Scripts/Pllayercontroler.cs
+++ johnlenon/Assets/Scripts/Pllayercontroler.cs
@@ -6,8 +6,16 @@ public class Pllayercontroler : MonoBehaviour
 {
     public float horizontalMove;
     public float verticallMove;
+
+    private Vector3 playerInput;
+
     public CharacterController player;
     public float playerspeed;
+    private Vector3 moveplayer;
+
+    public Camera mainCamera;
+    private Vector3 camForward;
+    private Vector3 camRight;
 
     void Start()
     {
@@ -17,13 +25,31 @@ public class Pllayercontroler : MonoBehaviour
     
     void Update()
     {
-        horizontalMove = Input.GetAxis("horizontal");
-        verticallMove = Input.GetAxis("vertical");
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticallMove = Input.GetAxis("Vertical");
 
 
+        playerInput = new Vector3(horizontalMove, 0, verticallMove);
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
+
+        camDiretion();
+        //moveplayer = playerInput.x * camRight * playerInput.z * camForward;
+
+        player.Move(playerInput * playerspeed * Time.deltaTime);
+        Debug.Log(player.velocity.magnitude);
     }
-    void FixedUpdate()
+    void camDiretion()  
     {
-        player.Move(new Vector3(horizontalMove, 0, verticallMove) * playerspeed);
+       
+        camForward = mainCamera.transform.forward;
+        camRight = mainCamera.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
     }
+
+
 }

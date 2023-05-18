a46acdab84ef5e8f2667d22bdd0b4a826411a6a8
basilio007 <113118989+basilio007@users.noreply.github.com>
2023-05-18 19:52:53 +0200

    uyu

--- johnlenon/Assets/Scripts/Platformcontolbase.cs
+++ johnlenon/Assets/Scripts/Platformcontolbase.cs
@@ -7,6 +7,11 @@ public class Platformcontolbase : MonoBehaviour
     public Rigidbody PlatformRB;
     public Transform[] PlatformPosition;
     public float PlatformSpeed;
+    private int actualposition = 0;
+    private int nextposition = 1;
+
+    public bool MoveToTheNext = true;
+    public float WaitTime;
 
     // Update is called once per frame
     void Update()
@@ -15,7 +20,30 @@ public class Platformcontolbase : MonoBehaviour
     }
    void MovePlatfom()
     {
-        PlatformRB.MovePosition(Vector3.MoveTowards(PlatformRB.position, PlatformPosition[1].position, PlatformSpeed * Time.deltaTime));
+        if (MoveToTheNext)
+        {
+            StopCoroutine(WaitForMove(0));
+            PlatformRB.MovePosition(Vector3.MoveTowards(PlatformRB.position, PlatformPosition[nextposition].position, PlatformSpeed * Time.deltaTime));
+        }
+       
 
+        if (Vector3.Distance(PlatformRB.position, PlatformPosition[nextposition].position) <= 0)
+        {
+            StartCoroutine(WaitForMove(WaitTime));
+            actualposition = nextposition;
+            nextposition++;
+
+            if (nextposition > PlatformPosition.Length - 0)
+            {
+                nextposition = 0;
+            }
+        }
+    }
+
+    IEnumerator WaitForMove(float time)
+    {
+        MoveToTheNext = false;
+        yield return new WaitForSeconds(time);
+        MoveToTheNext = true;
     }
 }
